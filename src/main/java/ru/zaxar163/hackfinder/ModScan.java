059909e461beabcf69554edee5c38bfdaf871c9e
Zaxar163 <zahar.vcherachny@yandex.ru>
2019-10-15 16:34:25 +0200

    Inital commit...

---
+++ src/main/java/ru/zaxar163/hackfinder/ModScan.java
@@ -0,0 +1,58 @@
+package ru.zaxar163.hackfinder;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+import java.util.Arrays;
+import java.util.Locale;
+import java.util.zip.ZipFile;
+
+import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.tree.AbstractInsnNode;
+import org.objectweb.asm.tree.ClassNode;
+import org.objectweb.asm.tree.MethodInsnNode;
+
+public class ModScan {
+	public static void main(final String[] args) throws FileNotFoundException {
+		final File mods = new File(args[0]);
+		try (PrintStream log = new PrintStream(new FileOutputStream(args[1]))) {
+			Arrays.stream(mods.listFiles()).filter(e -> e.getName().endsWith(".jar") || e.getName().endsWith(".zip"))
+					.map(e -> {
+						try {
+							return new ZipFile(e);
+						} catch (final Exception e1) {
+							throw new RuntimeException(e1);
+						}
+					}).forEach(e -> {
+						try {
+							System.out.println("Processing " + e.getName());
+							e.stream().filter(a -> a.getName().endsWith(".class")).forEach(a -> {
+								try {
+									final ClassNode n = new ClassNode();
+									new ClassReader(e.getInputStream(a)).accept(n, ClassReader.SKIP_FRAMES);
+									n.methods.forEach(u -> {
+										for (final AbstractInsnNode p1 : u.instructions.toArray())
+											if (p1 instanceof MethodInsnNode) {
+												final MethodInsnNode p = (MethodInsnNode) p1;
+												if (p.name.toLowerCase(Locale.US)
+														.contains("readItemStack".toLowerCase(Locale.US))) {
+													System.out.println("Class " + n.name
+															+ " may has packethack in method " + u.name);
+													log.println(new File(e.getName()).getName() + "=" + n.name + "#"
+															+ u.name);
+												}
+											}
+									});
+								} catch (final Throwable t) {
+									t.printStackTrace(System.err);
+								}
+							});
+						} catch (final Throwable t) {
+							throw new RuntimeException(t);
+						}
+					});
+			System.gc();
+		}
+	}
+}
